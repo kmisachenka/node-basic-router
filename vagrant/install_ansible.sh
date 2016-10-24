fcf6568a09c77dcd6c2f902cd1e3612373074752
kmisachenka <kmisachenka@gmail.com>
2016-10-24 22:06:58 +0300

    Add working files

--- vagrant/install_ansible.sh
+++ vagrant/install_ansible.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-echo "Installing Ansible..."
-apt-get install -y software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install -y --force-yes ansible
+dpkg -l ansible > /dev/null 2>&1
+INSTALLED=$?
+
+if [ $INSTALLED == '0' ]; then
+    echo "Ansible installed"
+else
+    echo "Installing Ansible..."
+    apt-get install -y software-properties-common
+    apt-add-repository ppa:ansible/ansible
+    apt-get update
+    apt-get install -y --force-yes ansible
+fi
+
+cp /vagrant/ansible.cfg /home/vagrant/ansible.cfg
+chown vagrant:vagrant /home/vagrant/ansible.cfg
+
+echo "ansible --version"
+ansible --version
